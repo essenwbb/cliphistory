1c03f9fb7ac043f6bd5444957397b4dc0071eb16
WuBinBin <1490779515@qq.com>
2019-05-01 08:12:42 +0800

    done hotkey

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-vgo/robotgo"
+	"github.com/wuyueCreator/cliphistory/hotkey"
+)
+
+var (
+	keys = map[int16]*hotkey.HotkeyAndEvent{
+		1: {&hotkey.Hotkey{1, hotkey.ModShift + hotkey.ModCtrl, 'V'}, test},
+	}
 )
 
 func main() {
 	globalDb = getDatabaseHandle()
 	defer globalDb.Close()
+
+	hotkey.KeysEventMap = keys
+	go hotkey.Run()
+
 	for {
 		if ok := robotgo.AddEvents("c", "ctrl"); ok {
 			recordClipToDatabase() //record to sqlite3
 		}
 	}
+}
+
+func test() {
+	fmt.Println("hello world")
 }
